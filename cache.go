28e980eba70bb6f76f8326a1c75eac5e2eccd8ea
Daniel Nephin <dnephin@gmail.com>
2023-09-24 00:18:14 -0400

    Build cache copy

--- cache.go
+++ cache.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -24,46 +25,26 @@ func fileName(id [HashSize]byte, key string) string {
 	return filepath.Join(fmt.Sprintf("%02x", id[0]), fmt.Sprintf("%x", id)+"-"+key)
 }
 
+var errNoOutputID = errors.New("no outputID")
+
 func readActionCacheFile(fh *os.File) (OutputID, error) {
-	outid := OutputID{}
+	outid := [HashSize]byte{}
 	if _, err := fh.Seek(0, 0); err != nil {
 		return outid, err
 	}
 
 	// putIndexEntry(id ActionID, out OutputID, size int64, allowVerify bool) error {
 	// entry := fmt.Sprintf("v1 %x %x %20d %20d\n", id, out, size, time.Now().UnixNano())
-	aid := ActionID{}
-	if _, err := fmt.Fscanf(fh, "v1 %x %x", &aid, &outid); err != nil {
+	var aid string
+	t := outid[:0]
+	n, err := fmt.Fscanf(fh, "v1 %v %x", &aid, &t)
+	if err != nil {
 		return outid, err
 	}
-	return outid, nil
-}
-
-// from src/cmd/internal/buildid/buildid.go
-
-// hashToString converts the hash h to a string to be recorded
-// in package archives and binaries as part of the build ID.
-// We use the first 120 bits of the hash (5 chunks of 24 bits each) and encode
-// it in base64, resulting in a 20-byte string. Because this is only used for
-// detecting the need to rebuild installed files (not for lookups
-// in the object file cache), 120 bits are sufficient to drive the
-// probability of a false "do not need to rebuild" decision to effectively zero.
-// We embed two different hashes in archives and four in binaries,
-// so cutting to 20 bytes is a significant savings when build IDs are displayed.
-// (20*4+3 = 83 bytes compared to 64*4+3 = 259 bytes for the
-// more straightforward option of printing the entire h in base64).
-
-// TODO: for fuzz testing stringToHash
-func hashToString(h [32]byte) string {
-	const b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
-	const chunks = 5
-	var dst [chunks * 4]byte
-	for i := 0; i < chunks; i++ {
-		v := uint32(h[3*i])<<16 | uint32(h[3*i+1])<<8 | uint32(h[3*i+2])
-		dst[4*i+0] = b64[(v>>18)&0x3F]
-		dst[4*i+1] = b64[(v>>12)&0x3F]
-		dst[4*i+2] = b64[(v>>6)&0x3F]
-		dst[4*i+3] = b64[v&0x3F]
+	// TODO: can this happen?
+	if n < 2 {
+		return outid, errNoOutputID
 	}
-	return string(dst[:])
+	// TODO: why isn't outid populated?
+	return OutputID(t[:HashSize]), nil
 }
--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"io/fs"
+	"log/slog"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -22,6 +26,9 @@ func run(args []string) error {
 	if err != nil {
 		return err
 	}
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})))
 
 	if err := copyBuildCache(opts); err != nil {
 		return err
@@ -63,41 +70,88 @@ func copyBuildCache(opts options) error {
 		return err
 	}
 
+	var count int
+	var size int64
 	for _, pkg := range pkgs {
-		aid, err := base64.URLEncoding.DecodeString(pkg.ActionID)
-		if err != nil {
-			return err
-		}
-		fmt.Println(aid, len(aid))
+		logger := slog.With("pkg", pkg.Package, "mode", pkg.Mode)
 
-		filename := fileName(ActionID(aid), "a")
-		fmt.Println(filename)
-		srcFH, err := copyFile(filepath.Join(toDir, filename), filepath.Join(fromDir, filename))
+		if pkg.ActionID == "" {
+			logger.Warn("no action ID")
+			continue
+		}
+		filename, err := findActionCacheFile(fromDir, pkg.ActionID)
+		switch {
+		case errors.Is(err, fs.ErrNotExist):
+			logger.Warn("no file matched", "prefix", filename)
+			continue
+		case err != nil:
+			return err
+		}
+
+		srcFH, n, err := copyFile(filepath.Join(toDir, filename), filepath.Join(fromDir, filename))
 		if err != nil {
 			return err
 		}
+		count++
+		size += n
 
 		outputID, err := readActionCacheFile(srcFH)
 		srcFH.Close() // close after read
-		if err != nil {
+		switch {
+		case errors.Is(err, errNoOutputID):
+			logger.Warn("no output file")
+			continue
+		case err != nil:
 			return err
 		}
-		filename = fileName(outputID, "d")
 
-		srcFH, err = copyFile(filepath.Join(toDir, filename), filepath.Join(fromDir, filename))
+		filename = fileName(outputID, "d")
+		srcFH, n, err = copyFile(filepath.Join(toDir, filename), filepath.Join(fromDir, filename))
 		if err != nil {
 			return err
 		}
 		srcFH.Close()
+		count++
+		size += n
 	}
+	// TODO: humanize bytes
+	slog.Info("Copied go build cache", "files", count, "bytes", size)
 	return nil
+}
+
+func findActionCacheFile(cacheDir string, actionID string) (string, error) {
+	prefix, err := base64.URLEncoding.DecodeString(actionID)
+	if err != nil {
+		return "", err
+	}
+
+	subdir := fmt.Sprintf("%02x", prefix[0])
+	dir := filepath.Join(cacheDir, subdir)
+	file, err := os.Open(dir)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	names, err := file.Readdirnames(0)
+	if err != nil {
+		return "", err
+	}
+
+	filePrefix := fmt.Sprintf("%x", prefix)
+	for _, name := range names {
+		// TODO: check -a suffix as well?
+		if strings.HasPrefix(name, filePrefix) {
+			return filepath.Join(subdir, name), nil
+		}
+	}
+	return filePrefix, fs.ErrNotExist
 }
 
 type actionGraphPkg struct {
 	ActionID  string
 	Package   string
 	Mode      string
-	NeedBuild string
+	NeedBuild bool
 }
 
 func parseActionGraph(filename string) ([]actionGraphPkg, error) {
@@ -112,25 +166,29 @@ func parseActionGraph(filename string) ([]actionGraphPkg, error) {
 	return target, err
 }
 
-func copyFile(dest, src string) (*os.File, error) {
+func copyFile(dest, src string) (*os.File, int64, error) {
 	srcFH, err := os.Open(src)
 	if err != nil {
-		return nil, err
+		return nil, 0, err
 	}
+
+	os.MkdirAll(filepath.Dir(dest), 0755)
 	destFH, err := os.Create(dest)
 	if err != nil {
 		srcFH.Close()
-		return nil, err
+		return nil, 0, err
 	}
-	_, err = io.Copy(destFH, srcFH)
+	size, err := io.Copy(destFH, srcFH)
 	if err != nil {
 		srcFH.Close()
 		destFH.Close()
-		return nil, err
+		return nil, 0, err
 	}
 	if err := destFH.Close(); err != nil {
 		srcFH.Close()
-		return nil, err
+		return nil, 0, err
 	}
-	return srcFH, nil
+
+	slog.Debug("copied file", "size", size, "from", srcFH.Name(), "to", destFH.Name())
+	return srcFH, size, nil
 }
